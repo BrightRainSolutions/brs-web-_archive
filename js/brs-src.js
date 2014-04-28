6f1ed14b219bdad24ff4d16f1547d7e23e0b20b5
David Puckett <david@brightrain.com>
2014-04-28 16:44:48 -0400

    layout updates, content fixes, image updates

--- js/brs-src.js
+++ js/brs-src.js
@@ -11,18 +11,21 @@ function fireup() {
 var brs = {
 	config: { 
 		canvasBasemapURL: "http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer",
-		canvasReferenceURL: "http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Reference/MapServer"
+		canvasReferenceURL: "http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Reference/MapServer",
+        blogPostsUrl: "https://public-api.wordpress.com/rest/v1/sites/blog.brightrain.com/posts/?number=8"
 	},
 	map: {},
+    clients: {},
 	graphicsLayerClients: {},
 	graphicsLayerHighlightClients: {},
 	symbolClient: {},
 	symbolHighlightClient: {},
 	init: function() {
-		//ToDo: auto load blog posts
 		try
 		{
-			this.map = new esri.Map("map", {
+			//!!!ToDo: get this going, cors
+            //this.populateBlogPosts();
+            this.map = new esri.Map("map", {
 					basemap: "gray",
 					center: [-70, 42],
 					zoom: 4,
@@ -34,7 +37,6 @@ var brs = {
 				});
 			dojo.connect(brs.map, "onLoad", function () {
 				brs.map.spatialReference = new esri.SpatialReference({ wkid: 102100 });
-				//Some of the api stuff might be outdated in the new 3.9
 				brs.initFunctionality();
 			});
             $('.brs-tabs a').click(function (e) {
@@ -53,26 +55,25 @@ var brs = {
 	initFunctionality: function() {
 		try
 		{
-		/**/
-		brs.symbolClient = new esri.symbol.PictureMarkerSymbol('images/brsSuns2.png', 20, 20);
-		brs.graphicsLayerClients = new esri.layers.GraphicsLayer();
-		var rendererClients = new esri.renderer.SimpleRenderer(brs.symbolClient);
-		brs.graphicsLayerClients.setRenderer(rendererClients);
-		brs.map.addLayer(brs.graphicsLayerClients);
-		
-		brs.addClientGraphics();
-		
-		brs.symbolHighlightClient = new esri.symbol.PictureMarkerSymbol('images/brsSuns1.png', 40, 40);
-		brs.graphicsLayerHighlightClients = new esri.layers.GraphicsLayer();
-		var rendererHighlightClients = new esri.renderer.SimpleRenderer(brs.symbolHighlightClient);
-		brs.graphicsLayerHighlightClients.setRenderer(rendererHighlightClients);
-		brs.map.addLayer(brs.graphicsLayerHighlightClients);
-		
-		dojo.connect(brs.graphicsLayerClients, "onMouseOver", brs.clientGraphicHover);
-		dojo.connect(brs.graphicsLayerClients, "onMouseOut", brs.clientGraphicMouseOut);
-		dojo.connect(brs.graphicsLayerClients, "onClick", brs.clientGraphicClick);
-		
-		return true;
+            brs.symbolClient = new esri.symbol.PictureMarkerSymbol('images/brsSuns2.png', 20, 20);
+            brs.graphicsLayerClients = new esri.layers.GraphicsLayer();
+            var rendererClients = new esri.renderer.SimpleRenderer(brs.symbolClient);
+            brs.graphicsLayerClients.setRenderer(rendererClients);
+            brs.map.addLayer(brs.graphicsLayerClients);
+
+            brs.addClientGraphics();
+
+            brs.symbolHighlightClient = new esri.symbol.PictureMarkerSymbol('images/brsSuns1.png', 40, 40);
+            brs.graphicsLayerHighlightClients = new esri.layers.GraphicsLayer();
+            var rendererHighlightClients = new esri.renderer.SimpleRenderer(brs.symbolHighlightClient);
+            brs.graphicsLayerHighlightClients.setRenderer(rendererHighlightClients);
+            brs.map.addLayer(brs.graphicsLayerHighlightClients);
+
+            dojo.connect(brs.graphicsLayerClients, "onMouseOver", brs.clientGraphicHover);
+            dojo.connect(brs.graphicsLayerClients, "onMouseOut", brs.clientGraphicMouseOut);
+            dojo.connect(brs.graphicsLayerClients, "onClick", brs.clientGraphicClick);
+
+            return true;
 		}
 		catch(e)
 		{
@@ -80,53 +81,13 @@ var brs = {
 		}
 	},
 	addClientGraphics: function() {
-		var ptEPA = new esri.geometry.Point(-8575141.8743, 4706234.52, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptEPA, null, {"Name":"Environmental Protection Agency (EPA)"}));
-
-        var ptERD = new esri.geometry.Point(-8234492.59, 4975607.6621, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptERD, null, {"Name":"Episcopal Relief & Development"}));
-
-        var ptDenver = new esri.geometry.Point(-11686902, 4827811, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptDenver, null, {"Name":"Your Castle Real Estate"}));
-
-        var ptSeattle = new esri.geometry.Point(-13617706, 6041407, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptSeattle, null, {"Name":"City of Seattle"}));
-
-        //landover, md
-        var ptLandover = new esri.geometry.Point(-8557000, 4714355, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptLandover, null, {"Name":"Antares Group"}));
-
-        //albuquerque
-        var ptAlbuquerque = new esri.geometry.Point(-11868451, 4182705, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptAlbuquerque, null, {"Name":"Infrastructure Technologies"}));
-
-        //chicago
-        var ptChicago = new esri.geometry.Point(-9806868, 5114585, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptChicago, null, {"Name":"GAD Group"}));
-
-        //kirkland, wa
-        var ptKland = new esri.geometry.Point(-13600524, 6053733, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptKland, null, {"Name":"PACE Engineers"}));
-
-        //sedro-woolley, wa
-        var ptSedro = new esri.geometry.Point(-13607520, 6191540, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptSedro, null, {"Name":"City of Sedro-Woolley, WA"}));
-
-        //broomfield, co
-        var ptBfield = new esri.geometry.Point(-11695506, 4859445, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptBfield, null, {"Name":"Your Castle Real Estate"}));
-
-        //nyc
-        var ptNYC = new esri.geometry.Point(-8216918, 4968370, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptNYC, null, {"Name":"New York City Dept of Corrections"}));
-
-        //graphical data gig harbor
-        var ptGigHarbor = new esri.geometry.Point(-13645653, 5996026, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptGigHarbor, null, {"Name":"Graphical Data"}));
-
-        //mt baker snoq national forest
-        var ptMtBakerSnoqNF = new esri.geometry.Point(-13515483, 6009965, brs.map.spatialReference)
-        brs.graphicsLayerClients.add(new esri.Graphic(ptMtBakerSnoqNF, null, {"Name":"Mt Baker \ Snoqualmie National Forest"}));
+		//!!!ToDo: there is a repo on github where client geojson data is being maintained
+        //figure out how to access it directly, cors problems...
+        $.each(brs.clients.features, function(index, brsclient) {
+            var pt = new esri.geometry.Point(brsclient.geometry.coordinates[0], 
+                                             brsclient.geometry.coordinates[1],new esri.SpatialReference({ wkid: 4326 }))
+            brs.graphicsLayerClients.add(new esri.Graphic(pt, null, {"Name":brsclient.properties.Name}));
+        });
 	},
 	clientGraphicHover: function(evt) {
 		//We could add text or image here to indicate the name as well.
@@ -143,6 +104,23 @@ var brs = {
 	clientGraphicClick: function(evt){
 		var clientGraphic = evt.graphic;
 	},
+    populateBlogPosts: function() {
+        $.getJSON(brs.config.blogPostsUrl, function(data) {
+            $.each(data.posts, function(index, post) {
+            var item = "<li>" +
+            "<div class='blog-date'>" + 
+            post.date + "</div>" +
+			"<a href='" + post.URL + "' title='" + post.title + "'>" +
+			"<div class='blog-entry-pane'>" + post.title + "</div>" +
+			"</a>" +
+			"</li>";
+            $("#blogList").append(post);
+            });
+        });   
+    }
+    /*
+    ,
+    //ToDo: consider reviving this so the user knows the map is live
 	tabChild: function(childTab){
 		var thePoint;
 		if (childTab.title == "Bio"){
@@ -160,4 +138,5 @@ var brs = {
 			map.centerAndZoom(thePoint,14);
 		}
 	}
+    */
 }
