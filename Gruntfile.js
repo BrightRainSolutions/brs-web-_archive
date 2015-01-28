8acfbba3e18ffe0290ed610cdf7fbe89e0366d35
David Puckett <david@brightrain.com>
2015-01-28 13:52:23 -0700

    add brs hq and zoom to, html fixes, implement build system

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            src: ['js/cwns-core.js', 'js/cwns-config.js'],
+            src: ['js/brs.js'],
             //http://www.jshint.com/docs/options/
             options: {
                 curly: true,
@@ -16,69 +16,64 @@ module.exports = function(grunt) {
                 jquery: true
             }
         },
+        /*
         concat: {
             dist: {
-                src: ['js/cwns-core.js', 'js/cwns-config.js'],
-                dest: 'js/cwns-src.js',
+                src: ['js/brs-core.js', 'js/brs-config.js'],
+                dest: 'js/brs-src.js',
             }
-        },
+        },*/
         uglify: {
             build: {
                 //the output from concat above
-                src: 'js/cwns-src.js',
-                dest: 'js/cwns.min.js'
+                src: 'js/brs.js',
+                dest: 'js/brs.min.js'
             }
         },
         //https://npmjs.org/package/grunt-contrib-copy
         copy: {
-          main: {
-            options: {
-                //replace the reference to cwns.js in our dev to our minified version for build
-                //ToDo: verify that this happens after we copy the file even tho it is above the files array
-                process: function (content, srcpath) {
-                    return content.replace("cwns-src.js","cwns.min.js");
-                  }
+            main: {
+                options: {
+                    processContentExclude: [
+                        '**/*.{pdf,png,gif,jpg,ico,psd}'
+                    ],
+                    //replace the reference to brs.js in our dev to our minified version for build
+                    process: function (content, srcpath) {
+                        return content.replace("brs.js","brs.min.js");
+                    }
                 },
-              files: [
-                {
-                    //copy our index to the root of our distribution dir
-                    src: 'index.html',
-                    dest: 'dist/',
-                },
-                //copy our minified js file to distribution 'js' dir
-                {
-                    expand: true,
-                    cwd: 'js',
-                    src: ['cwns.min.js'],
-                    dest: 'dist/js',
-                    filter: 'isFile'
-                },
-                  //copy all of our lib folders and files to distribution
-                {
-                    expand: true,
-                    cwd: 'js/lib',
-                    src: ['**/*.js'],
-                    dest: 'dist/js/lib',
-                    filter: 'isFile'
-                },
-                  //copy all of our css folders and files to distribution
-                {
-                    expand: true,
-                    cwd: 'css',
-                    src: ['**/*.css'],
-                    dest: 'dist/css',
-                    filter: 'isFile'
-                },
-                  //copy all of our img folders and files to distribution
-                {
-                    expand: true,
-                    cwd: 'img',
-                    src: ['**/*'],
-                    dest: 'dist/img',
-                    filter: 'isFile'
-                },
-            ]
-          }
+                files: [
+                    {
+                        //copy our index to the root of our distribution dir
+                        src: 'index.html',
+                        dest: 'dist/',
+                    },
+                    //copy our minified js file to distribution 'js' dir
+                    {
+                        expand: true,
+                        cwd: 'js',
+                        src: ['brs.min.js', 'brs-clients.geojson', 'slimbox2.js'],
+                        dest: 'dist/js',
+                        filter: 'isFile'
+                    },
+                    //copy all of our style folders and files to distribution
+                    {
+                        expand: true,
+                        cwd: 'style',
+                        src: ['**/*.css'],
+                        dest: 'dist/style',
+                        filter: 'isFile'
+                    },
+                    //copy all of our img folders and files to distribution
+                    {
+                        expand: true,
+                        cwd: 'images',
+                        src: ['**/*'],
+                        dest: 'dist/images',
+                        filter: 'isFile'
+                    },
+                ]
+            }
         },
         //use watch to monitor changes to any of our js files
         //if so run concat and min so our app always has the latest
@@ -98,8 +93,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     /*watch*/
     grunt.loadNpmTasks('grunt-contrib-watch');
-    //!!!ToDo: this copy operation doesn't copy the images correctly
-    grunt.registerTask('build', ['jshint', 'concat', 'uglify', 'copy']);
-    grunt.registerTask('dev', ['concat', 'watch']);
-    grunt.registerTask('default', ['concat', 'uglify', 'copy']);
+    //grunt.registerTask('build', ['jshint', 'concat', 'uglify', 'copy']);
+    grunt.registerTask('build', ['jshint', 'uglify', 'copy']);
+    //grunt.registerTask('dev', ['concat', 'watch']);
+    grunt.registerTask('default', ['uglify', 'copy']);
 };
--- js/brs.js
+++ js/brs.js
@@ -1,2 +1,165 @@
-/*! brsweb 2013-10-14 */
-function fireup(){$(document).ready(brs.init())}dojo.require("esri.map"),dojo.require("esri.layers.WebTiledLayer"),dojo.addOnLoad(fireup);var brs={config:{canvasBasemapURL:"http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer",canvasReferenceURL:"http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Reference/MapServer"},map:{},graphicsLayerClients:{},graphicsLayerHighlightClients:{},symbolClient:{},symbolHighlightClient:{},init:function(){return this.map=new esri.Map("map",{basemap:"gray",center:[-90,35],zoom:4,slider:!1,logo:!1}),dojo.connect(brs.map,"onLoad",function(){brs.map.spatialReference=new esri.SpatialReference({wkid:102100})}),$("#preloader").hide(),!0},initFunctionality:function(){},addClientGraphics:function(){var a=new esri.geometry.Point(-80,40,map.spatialReference);this.graphicsLayerClients.add(new esri.Graphic(a))},clientGraphicHover:function(a){graphicsLayerHighlightClients.add(a.graphic),map.setMapCursor("pointer")},clientGraphicMouseOut:function(a){map.setMapCursor("default"),graphicsLayerHighlightClients.clear(),graphicsLayerClients.add(new esri.Graphic(a.graphic))},clientGraphicClick:function(a){a.graphic},tabChild:function(a){var b;"Bio"==a.title?(b=new esri.geometry.Point(-8578275,4705330,map.spatialReference),map.centerAndZoom(b,14)):"Clients"==a.title?(b=new esri.geometry.Point(-8587230,4715450,map.spatialReference),map.centerAndZoom(b,4)):"Projects"==a.title?(b=new esri.geometry.Point(-13618975,6041570,map.spatialReference),map.centerAndZoom(b,15)):"Bright Rain Solutions"==a.title&&(b=new esri.geometry.Point(-8577230,4705450,map.spatialReference),map.centerAndZoom(b,14))}};dojo.declare("wax.esri.connector",esri.layers.TiledMapServiceLayer,{constructor:function(a){a=a||{},this.options={tiles:a.tiles,minzoom:a.minzoom||0,maxzoom:a.maxzoom||22};var b=20037508.342789;this.spatialReference=new esri.SpatialReference({wkid:3857}),this.initialExtent=new esri.geometry.Extent(-b,-b,b,b,this.spatialReference),this.fullExtent=this.initialExtent;for(var c=[],d=this.options.minzoom;d<=this.options.maxzoom;d++)c.push({level:d,scale:591657527.591555/Math.pow(2,d),resolution:156543.033928/Math.pow(2,d)});this.tileInfo=new esri.layers.TileInfo({spatialReference:{wkid:"3857"},rows:256,cols:256,origin:{x:-b,y:b},lods:c}),this.loaded=!0,this.onLoad(this)},getTileUrl:function(a,b,c){return this.options.tiles[parseInt(Math.pow(2,a)*b+c,10)%this.options.tiles.length].replace("{z}",a).replace("{x}",c).replace("{y}",b)}});
+dojo.require("esri.map");
+dojo.require("esri.layers.WebTiledLayer");
+dojo.require("esri.symbols.PictureMarkerSymbol");
+dojo.require("esri.renderers.SimpleRenderer");
+dojo.require("esri.graphic");
+dojo.addOnLoad(fireup);
+function fireup() {
+    //setup our app ui when the doc is ready
+    $(document).ready(brs.init());
+}
+var brs = {
+    config: { 
+        canvasBasemapURL: "http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer",
+        canvasReferenceURL: "http://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Reference/MapServer",
+        blogPostsUrl: "https://public-api.wordpress.com/rest/v1/sites/blog.brightrain.com/posts/?number=8"
+    },
+    map: {},
+    clients: {},
+    graphicsLayerClients: {},
+    graphicsLayerHighlightClients: {},
+    symbolClient: {},
+    symbolHighlightClient: {},
+    init: function() {
+        try
+        {
+            //!!!ToDo: get this going, cors
+            //this.populateBlogPosts();
+            this.map = new esri.Map("map", {
+                basemap: "gray",
+                center: [-70, 42],
+                zoom: 4,
+                slider: false,
+                //sliderStyle: "small",
+                //sliderOrientation: "horizontal",
+                //sliderPosition: "bottom-left",
+                logo: false
+            });
+            dojo.connect(brs.map, "onLoad", function () {
+                brs.map.spatialReference = new esri.SpatialReference({ wkid: 102100 });
+                brs.initFunctionality();
+            });
+            $('.brs-tabs a').click(function (e) {
+                e.preventDefault();
+                $(this).tab('show');
+            });
+            $("#about-tab").click(function() {
+                brs.ZoomToHQ();
+            });
+            //Hide the div that we display while loading the page.
+            $("#preloader").hide();
+            return true;
+        }
+        catch(e)
+        {
+            return false;
+        }
+    },
+    initFunctionality: function() {
+        try
+        {
+            var symbolHQ = new esri.symbol.PictureMarkerSymbol('images/brsSun.png', 23, 23);
+            this.map.graphics.add(new esri.Graphic(new esri.geometry.Point(-105.075739, 40.587838, new esri.SpatialReference({ wkid: 4326 })), symbolHQ));
+
+            brs.symbolClient = new esri.symbol.PictureMarkerSymbol('images/brsSuns2.png', 20, 20);
+            brs.graphicsLayerClients = new esri.layers.GraphicsLayer();
+            var rendererClients = new esri.renderer.SimpleRenderer(brs.symbolClient);
+            brs.graphicsLayerClients.setRenderer(rendererClients);
+            brs.map.addLayer(brs.graphicsLayerClients);
+
+            brs.addClientGraphics();
+
+            brs.symbolHighlightClient = new esri.symbol.PictureMarkerSymbol('images/brsSuns1.png', 40, 40);
+            brs.graphicsLayerHighlightClients = new esri.layers.GraphicsLayer();
+            var rendererHighlightClients = new esri.renderer.SimpleRenderer(brs.symbolHighlightClient);
+            brs.graphicsLayerHighlightClients.setRenderer(rendererHighlightClients);
+            brs.map.addLayer(brs.graphicsLayerHighlightClients);
+
+            dojo.connect(brs.graphicsLayerClients, "onMouseOver", brs.clientGraphicHover);
+            dojo.connect(brs.graphicsLayerClients, "onMouseOut", brs.clientGraphicMouseOut);
+            dojo.connect(brs.graphicsLayerClients, "onClick", brs.clientGraphicClick);
+
+            return true;
+        }
+        catch(e)
+        {
+            return false;
+        }
+    },
+    ZoomToHQ: function() {
+        try
+        {
+            this.map.setExtent(new esri.geometry.Extent({
+                "xmin":-105.07,
+                "ymin":40.5866,
+                "xmax":-105.063,
+                "ymax":40.5914,
+                "spatialReference":new esri.SpatialReference({ wkid: 4326 })
+            }),true);
+            return true;
+        }
+        catch(e)
+        {
+            return false;
+        }
+    },
+    addClientGraphics: function() {
+        //!!!ToDo: there is a repo on github where client geojson data is being maintained
+        //figure out how to access it directly, cors problems...
+        $.each(brs.clients.features, function(index, brsclient) {
+            var pt = new esri.geometry.Point(brsclient.geometry.coordinates[0], 
+                                             brsclient.geometry.coordinates[1],brs.map.SpatialReference);
+            brs.graphicsLayerClients.add(new esri.Graphic(pt, null, {"Name":brsclient.properties.Name}));
+        });
+    },
+    clientGraphicHover: function(evt) {
+        //We could add text or image here to indicate the name as well.
+        brs.graphicsLayerHighlightClients.add(evt.graphic);
+        brs.map.setMapCursor("pointer");
+        dojo.byId("client-info").innerHTML = evt.graphic.attributes.Name;
+    },
+    clientGraphicMouseOut: function(evt) {
+        brs.map.setMapCursor("default");
+        brs.graphicsLayerHighlightClients.clear();
+        brs.graphicsLayerClients.add(evt.graphic);
+        dojo.byId("client-info").innerHTML = "";
+    },
+    clientGraphicClick: function(evt){
+        var clientGraphic = evt.graphic;
+    },
+    populateBlogPosts: function() {
+        $.getJSON(brs.config.blogPostsUrl, function(data) {
+            $.each(data.posts, function(index, post) {
+                var item = "<li>" +
+                    "<div class='blog-date'>" + 
+                    post.date + "</div>" +
+                    "<a href='" + post.URL + "' title='" + post.title + "'>" +
+                    "<div class='blog-entry-pane'>" + post.title + "</div>" +
+                    "</a>" +
+                    "</li>";
+                $("#blogList").append(post);
+            });
+        });   
+    }
+    /*
+    ,
+    //ToDo: consider reviving this so the user knows the map is live
+	tabChild: function(childTab){
+		var thePoint;
+		if (childTab.title == "Bio"){
+			thePoint = new esri.geometry.Point(-8578275, 4705330, map.spatialReference);
+			map.centerAndZoom(thePoint, 14);
+			//dojo.style("header", "height", "600px")
+		}else if (childTab.title == "Clients") {
+			thePoint = new esri.geometry.Point(-8587230, 4715450, map.spatialReference);
+			map.centerAndZoom(thePoint, 4);
+		}else if (childTab.title == "Projects") {
+			thePoint = new esri.geometry.Point(-13618975, 6041570, map.spatialReference);
+			map.centerAndZoom(thePoint,15);
+		}else if (childTab.title == "Bright Rain Solutions") {
+			thePoint = new esri.geometry.Point(-8577230, 4705450, map.spatialReference);
+			map.centerAndZoom(thePoint,14);
+		}
+	}
+    */
+};
